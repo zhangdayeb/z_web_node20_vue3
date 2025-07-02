0d4d1ae67751294a93a61d8222e8fbbf1137f738
zhangdayeb <you@example.com>
2025-07-02 16:05:41 +0800

    'up'

--- vite.config.ts
+++ vite.config.ts
@@ -81,12 +81,12 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://bw1.123456.top/',
+        target: 'https://m.9zhou.top/', // http://bw1.123456.top
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '/api'),
       },
       '/storage': {
-        target: 'http://bw1.123456.top/',
+        target: 'https://m.9zhou.top/',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/storage/, '/storage/'),
       },
