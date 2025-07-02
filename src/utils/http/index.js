7c7976c87509db4cdebf5941000da5eeaad2a3ce
zhangdayeb <you@example.com>
2025-07-02 16:17:57 +0800

    'up'

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import { setupInterceptors } from './interceptors';
 
+// 改良版本
 export function createAxios(options = {}) {
   const defaultOptions = {
-    baseURL: '/api',
+    // 根据环境变量决定 baseURL
+    baseURL: import.meta.env.VITE_API_BASE_URL ?
+      `${import.meta.env.VITE_API_BASE_URL}/api` :
+      '/api',
     timeout: 12000,
   };
   const service = axios.create({
@@ -13,6 +17,19 @@ export function createAxios(options = {}) {
   setupInterceptors(service);
   return service;
 }
+// 默认版本
+// export function createAxios(options = {}) {
+//   const defaultOptions = {
+//     baseURL: '/api',
+//     timeout: 12000,
+//   };
+//   const service = axios.create({
+//     ...defaultOptions,
+//     ...options,
+//   });
+//   setupInterceptors(service);
+//   return service;
+// }
 
 export const request = createAxios();
 
